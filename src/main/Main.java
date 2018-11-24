2cf0a9316109121be5af4b51662d48fc0ac9bdfc
Robson Marinho <robson_luz15@hotmail.com>
2018-11-24 12:18:09 -0300

    Comentando códigos

--- src/main/Main.java
+++ src/main/Main.java
@@ -9,29 +9,26 @@ import java.sql.SQLException;
 public class Main {
 
 	public static void main(String[] args) {
-		
+		// Insere o Db de conexão e o caminho onde se encontra o arquivo
 		String connectionURL = "JDBC:SQLITE:C:/Users/Robson Marinho/Downloads/sqlite/chinook.db";
 		try {
 			Connection conn = DriverManager.getConnection(connectionURL);
-		
+
 			System.out.println("Conectou ao SQLite");
-		
-		
+			// Query na tabela genres
 			PreparedStatement stm = conn.prepareStatement("SELECT * FROM genres;");
 			ResultSet res = stm.executeQuery();
-			
+
 			while (res.next()) {
-				
+
 				Integer genreId = res.getInt("GenreId");
 				String genreName = res.getString("Name");
-				
-				System.out.println(
-						genreId + " | " + genreName 
-				);
+				// Exibe as colunas
+				System.out.println(genreId + " | " + genreName);
 			}
-			
+
 		} catch (SQLException e) {
-			// TODO Auto-generated catch block
+
 			e.printStackTrace();
 		}
 	}
